8d9527236b35e4a730fae845c645d26329cd9aa0
Tatiana <tatianalimongi@gmail.com>
2025-04-20 02:03:16 -0300

    chore(codeql): justify false positive on GlobalExceptionHandler

--- src/main/java/com/example/userform/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/userform/exception/GlobalExceptionHandler.java
@@ -3,7 +3,8 @@ package com.example.userform.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,6 +12,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	// codeql [java/unreachable-handler-method]: Justified - Spring instantiates this class automatically via @RestControllerAdvice
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
 		Map<String, String> errors = new HashMap<>();
